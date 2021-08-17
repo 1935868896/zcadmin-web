537200d57018644fb5690bfa765d9514935ba420
ZhangChen <1935868896@qq.com>
2021-08-17 14:09:38 +0800

    本次提交

---
+++ src/router/modules/customize.js
@@ -0,0 +1,41 @@
+/** When your routing table is too long, you can split it into small modules **/
+
+import Layout from '@/layout'
+
+const tableRouter = {
+  path: '/customize',
+  component: Layout,
+  redirect: '/customize/complex-table',
+  name: '自定义',
+  meta: {
+    title: '自定义',
+    icon: 'table'
+  },
+  children: [
+    {
+      path: 'dynamic-table',
+      component: () => import('@/views/customize/dynamic-table/index'),
+      name: 'DynamicTable',
+      meta: { title: 'Dynamic Table' }
+    },
+    {
+      path: 'drag-table',
+      component: () => import('@/views/customize/drag-table'),
+      name: 'DragTable',
+      meta: { title: 'Drag Table' }
+    },
+    {
+      path: 'inline-edit-table',
+      component: () => import('@/views/customize/inline-edit-table'),
+      name: 'InlineEditTable',
+      meta: { title: 'Inline Edit' }
+    },
+    {
+      path: 'complex-table',
+      component: () => import('@/views/customize/complex-table'),
+      name: '文章',
+      meta: { title: '文章' }
+    }
+  ]
+}
+export default tableRouter
