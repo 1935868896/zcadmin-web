e4e19f678464b7b280b29c62bd45aea92de9ca5c
ZhangChen <1935868896@qq.com>
2021-11-02 15:46:33 +0800

    本次提交: 对vue打包进行gzip的压缩

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 'use strict'
 const path = require('path')
 const defaultSettings = require('./src/settings.js')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
 
 function resolve(dir) {
   return path.join(__dirname, dir)
@@ -46,7 +48,16 @@ module.exports = {
       alias: {
         '@': resolve('src')
       }
-    }
+    },
+    plugins: [
+      new CompressionWebpackPlugin({
+        filename: '[path].gz[query]', // 提示compression-webpack-plugin@3.0.0的话asset改为filename
+        algorithm: 'gzip',
+        test: new RegExp('\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    ]
   },
   chainWebpack(config) {
     // it can improve the speed of the first screen, it is recommended to turn on preload
